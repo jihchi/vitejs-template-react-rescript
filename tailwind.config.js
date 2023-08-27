72e3e0bd9dfb1fd31fbdc7786947f1c9dd6bc857
jihchi <achi@987.tw>
2023-08-27 15:13:33 +0200

    Upgrade dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.bs.js"],
+  content: ['./index.html', './src/**/*.bs.js'],
   theme: {
     extend: {},
   },
